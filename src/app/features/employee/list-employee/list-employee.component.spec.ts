6a94a32c54821c83d1c511ae28acf6a0429c4ab7
Adrita Sharma <adritasharma.developer@gmail.com>
2021-05-03 13:19:18 +0530

    mock service unit test case written

--- src/app/features/employee/list-employee/list-employee.component.spec.ts
+++ src/app/features/employee/list-employee/list-employee.component.spec.ts
@@ -1,32 +1,49 @@
 /* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ListEmployeeComponent } from './list-employee.component';
 import { EmployeeService } from '../employee.service';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { of } from 'rxjs';
 
 describe('ListEmployeeComponent', () => {
   let component: ListEmployeeComponent;
   let fixture: ComponentFixture<ListEmployeeComponent>;
+  let mockEmployeeService
+  let employeeSampleData;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports:[HttpClientModule],
-      declarations: [ ListEmployeeComponent ],
-      providers:[EmployeeService]
-    })
-    .compileComponents();
-  }));
 
   beforeEach(() => {
+    employeeSampleData = [
+      { employeeId: 1, fullName: "Adrita Sharma" },
+      { employeeId: 2, fullName: "Deepika Das" },
+      { employeeId: 3, fullName: "Ankita Sarkar" }
+    ]
+    mockEmployeeService = jasmine.createSpyObj(['getAllEmployees'])
+
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      declarations: [ListEmployeeComponent],
+      providers: [
+        { provide: EmployeeService, useValue: mockEmployeeService }
+      ]
+    })
+      .compileComponents();
+
     fixture = TestBed.createComponent(ListEmployeeComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should set employees correctly from the service', () => {
+    mockEmployeeService.getAllEmployees.and.returnValue(of(employeeSampleData));
+    fixture.detectChanges();
+    //component.ngOnInit()
+
+    expect(mockEmployeeService.getAllEmployees).toHaveBeenCalled();
+    expect(component.employees.length).toBe(3);
   });
 });
